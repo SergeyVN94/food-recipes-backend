4abeb890acefbb7f71f256273aa24780f093c474
SergeyVN94 <blakdgak@yandex.ru>
2022-11-30 20:43:40 +0300

    Начальный коммит.

---
+++ src/modules/app/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/api/server-time')
+  getApiA(): string {
+    return this.appService.getServerTime();
+  }
+}
